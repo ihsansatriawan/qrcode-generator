1e63864f36edd3d63a5bdb13d6bad85ad6ca603b
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2019-10-19 15:10:47 +0700

    Change minor

--- index.js
+++ index.js
@@ -11,31 +11,22 @@ app.get('/start_generate', (req, res, next) => {
   const names = ['Ihsan Satriawan', 'Ferly Fer']
 
   names.forEach(name => {
-    const qr_png = qr.imageSync(name, { type: 'png' })
+    const qr_png = qr.imageSync(name, { type: 'png', ec_level: 'H', margin: 1 })
     const qr_code_file_name = name + '.png';
 
-    fs.writeFile(('./public/qr/' + qr_code_file_name), qr_png, (res) => {
-      if (res) {
-        console.log("Error: ", res, " ", qr_code_file_name);
-      } else {
-        console.log("Sukses: ", qr_code_file_name);
-      }
-    })
+    try {
+      fs.writeFile(('./public/qr/' + qr_code_file_name), qr_png, (res) => {
+        if (res) {
+          console.log("Error: ", res, " ", qr_code_file_name);
+        } else {
+          console.log("Sukses: ", qr_code_file_name);
+        }
+      }) 
+    } catch (error) {
+      console.log("Error Catch: ", res, " ", qr_code_file_name);
+    }
 
   })
-  // const qr_txt = "Ihsan Satriawan"
-
-  // const qr_png = qr.imageSync(qr_txt, { type: 'png' })
-  // const qr_code_file_name = qr_txt + '.png';
-
-  // fs.writeFile(('./public/qr/' + qr_code_file_name), qr_png, (res) => {
-  //   console.log("res: ", res)
-  //   if (res) {
-  //     console.log(err);
-  //   } else {
-  //     console.log("Sukses: ", qr_code_file_name);
-  //   }
-  // })
 
   res.send("Attemp")
 })
